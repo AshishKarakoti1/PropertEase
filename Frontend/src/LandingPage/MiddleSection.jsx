76e67e1ac42918d75d0336866dfd422a1e57163f
Daksh_Chawla <dakshf219@gmail.com>
2024-08-10 19:53:30 +0530

    added typing animation

--- Frontend/src/LandingPage/MiddleSection.jsx
+++ Frontend/src/LandingPage/MiddleSection.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import styles from './Middle.module.css';
+import Typed from 'typed.js';
+import { useRef, useEffect } from 'react';
 
 const MiddleSection = () => {
+
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['Buying', 'Selling'],  // Text strings to be typed
+            typeSpeed: 100,            // Speed of typing in milliseconds
+            backSpeed: 100,            // Speed of backspacing
+            loop: true,               // Loop the typing effect
+            loopCount: Infinity,      // Repeat the loop indefinitely
+            startDelay: 500,          // Delay before typing starts
+            backDelay: 500,           // Delay before backspacing
+        });
+
+        return () => {
+            // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+        };
+    }, []);
+
     return (
         <div className='flex justify-center p-8 gap-5 items-start'>
             <div className='flex-1 max-w-3xl'>
                 <h1 className='text-6xl font-bold leading-tight mb-4 text-white'>
-                    Buying & Selling <br /> properties made easy
+                    <span ref={el}></span> <br /> properties made easy
                 </h1>
                 <p className='mt-2 text-white text-xl'>
                     Now, everything you need to successfully buy or sell your home is on <br /> one platform.
